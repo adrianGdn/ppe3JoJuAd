31ac297a7995958eed833c07f3b86562ac13d3ea
adrianGdn <adrian.gandon@epsi.fr>
2016-12-13 14:13:15 +0100

    Revert "maquette liste frais forfait + hors forfait"
    This reverts commit 47aa6d324573d1315652b36e39e14e62a80de608.

--- gsbAppliFraisV2/vues/v_listeFraisForfait.php
+++ gsbAppliFraisV2/vues/v_listeFraisForfait.php
@@ -3,111 +3,27 @@
 		<div class="panel-heading">
 			<div class="panel-title"><h2></h2></div>
 			</br></br>
-            <p> Renseigner la fiche du mois de :
-                <select method="POST" name="fromSelectMois">
-                    <option value="Janvier">Janvier</option>
-                    <option value="Fevrier">Fevrier</option>
-                    <option value="Mars">Mars</option>
-                    <option value="Avril">Avril</option>
-                    <option value="Mais">Mai</option>
-                    <option value="Juin">Juillet</option>
-                    <option value="Aout">Aout</option>
-                    <option value="Septembre">Septembre</option>
-                    <option value="Octobre">Octobre</option>
-                    <option value="Novembre">Novembre</option>
-                    <option value="Decembre">Decembre</option>
-                </select>
-            </p>
+		    <legend>Eléments forfaitisés</legend>
 		</div>
-
-        <div class="panel-body">
-            <form class="form-horizontal" role="form" method="POST"  action="index.php?uc=gererFrais&action=validerMajFraisForfait">
-                <label>Saisie d'un nouveau frais forfaitisé :</label><br/>
-                <label>Type de frais :</label>
-                <select method="POST" name="lstFrais" id="typeFrais"><option selected="choix-Option"> Choisissez un type de frais
-                        <?php foreach ($lesFraisForfait as $libelleFrais)
-                        { ?>
-                    <option value="<? echo $libelleFrais['libelle']?>"><?php echo $libelleFrais['libelle'] ?></option>
-                    <?php
-                        }
-                        ?>
-                </select> <br/>
-
-                <label>Date de l'engagement de la dépense :</label>
-                <form name="Filter" method="POST">
-                    <input type="date" name="dateAjout" value="<?php echo date('Y-m-d'); ?>" />
-            </form>
-            <br/>
-            <label>Description :</label> <input method="POST" name="descriptionInput" type="text"><br/>
-            <label>Quantité :</label> <input method="POST" name="quantitéInput" type="number"><br/>
-                <input class="btn btn-primary" id="ok" type="submit" value="Valider" size="20" <?php if ($lesInfosFicheFrais['idEtat']!='CR') { echo 'disabled';} ?>/> <br/>
-            </form>
-        </div>
-
 		<div class="panel-body">
-            <label name="lblElemForf">Eléments forfaitisés (synthèse du mois)</label><br/>
 			<form class="form-horizontal" role="form" method="POST"  action="index.php?uc=gererFrais&action=validerMajFraisForfait">
 			    <div class="form-group">
                     <?php
-                        foreach ($lesFraisForfait as $unFrais) {
+                        foreach ($lesFraisForfait as $unFrais)
+                        {
                             $idFrais = $unFrais['idfrais'];
                             $libelle = $unFrais['libelle'];
                             $quantite = $unFrais['quantite'];
-                            //$montantTotal = $unFrais['MontantTotal']
-                        }
-
                     ?>
-
-                    <table class="tableau">
-                        <thead>
-                        <tr><td><?php echo "  ";?></td><td><?=implode("</td><td>",array_column($lesFraisForfait, "libelle"))?></td></tr>
-                        </thead>
-                        <tbody>
-                        <tr><td>Quantité Totale</td><td><?=implode("</td><td>",array_column($lesFraisForfait, "idfrais"))?></td></tr>
-                        <tr><td>Montant Total</td><td><?=implode("</td><td>",array_column($lesFraisForfait, "quantite"))?></td></tr>
-                        </tbody>
-                    </table>
-
-                    <label>Total des frais forfaitisés engagés pour le mois : <?php echo " " ?></label> <br/>
-
-                    <label>Elements forfaitisés (détails du mois) :</label> <br/>
-                    <?php ?>
-                    <table class="tableau">
-                        <thead>
-                        <tr>
-                            <td>Date</td>
-                            <td>Type frais</td>
-                            <td>Description</td>
-                            <td>Quantité</td>
-                        </tr>
-                        </thead>
-                        <tbody>
-                      <!--  <tr><td><?=implode("</td><td>",array_column($lesInfosFicheFrais,"dateModif"))?></td></tr> trouver pourquoi ne fonctionne pas -->
-                        <tr>
-                            <td><?php echo $lesInfosFicheFrais[1] ?></td>
-                            <td><?php echo $libelle ?></td>
-                            <td><?php echo " " ?></td>
-                            <td><?php echo $quantite ?></td>
-                        </tr>
-
-                        </tbody>
-                    </table>
-                    <br/>
-
-                    <input class="btn btn-primary" id="ok" type="submit" value="Valider" size="20" <?php if ($lesInfosFicheFrais['idEtat']!='CR') { echo 'disabled';} ?>/>
-
                     <div class="form-group">
-
-					   <!-- <label for="idFrais"><?php echo $libelle ?></label> -->
-
+					    <label for="idFrais"><?php echo $libelle ?></label>
 					    <input class="form-control" placeholder="<?php echo $quantite?>" type="text" id="idFrais" name="lesFrais[<?php echo $idFrais?>]""<?php echo $quantite?>"<?php if ($lesInfosFicheFrais['idEtat']!='CR') { echo 'disabled';} ?> />
 					</div>
 					<?php
-
+						}
 					?>
-
 				</div>
-
+				<input class="btn btn-primary" id="ok" type="submit" value="Valider" size="20" <?php if ($lesInfosFicheFrais['idEtat']!='CR') { echo 'disabled';} ?>/>
             </form>
         </div>
     </div>
--- gsbAppliFraisV2/vues/v_listeFraisHorsForfait.php
+++ gsbAppliFraisV2/vues/v_listeFraisHorsForfait.php
@@ -1,91 +1,4 @@
 ﻿<div class="col-md-6">
-    <div class="content-box-large">
-        <div class="panel-heading">
-            <legend>Nouvel élément hors forfait</legend>
-        </div>
-        <div class="panel-body">
-
-            <!-- Code Avant modification interface graphique -->
-
-          <!--  <form class="form-horizontal" role="form" action="index.php?uc=gererFrais&action=validerCreationFrais" method="post">
-                <div class="form-group">
-                    <div class="form-group">
-                        <label for="txtDateHF"> Date (jj/mm/aaaa): </label>
-                        </br>
-                        <input class="form-control" type="text" id="txtDateHF" name="dateFrais" <?php if ($lesInfosFicheFrais['idEtat']!='CR') { 		echo 'disabled';};   ?>/>
-                        </br></br>
-                        <label for="txtLibelleHF">Libellé</label>
-                        </br>
-                        <input class="form-control" type="text" id="txtLibelleHF" name="libelle"  <?php if ($lesInfosFicheFrais['idEtat']!='CR') { 		echo 'disabled';};   ?>/>
-                        </br></br>
-                        <label for="txtMontantHF">Montant : </label>
-                        </br>
-                        <input class="form-control" type="text" id="txtMontantHF" name="montant"  <?php if ($lesInfosFicheFrais['idEtat']!='CR') { 		echo 'disabled';};   ?>/>
-                        </br></br>
-                    </div>
-                </div>
-                <div class="horizontal-form">
-                    <input class="btn btn-primary" id="ajouter" type="submit" value="Ajouter" <?php if ($lesInfosFicheFrais['idEtat']!='CR') { echo 'disabled';} ?> />
-                    <input class="btn btn-primary" id="effacer" type="reset" value="Effacer"/>
-                </div>
-            </form> -->
-
-            <!-- fin code original -->
-
-            </br>
-            <p> Renseigner la fiche du mois de :
-                <select method="POST" name="fromSelectMois">
-                    <option value="Janvier">Janvier</option>
-                    <option value="Fevrier">Fevrier</option>
-                    <option value="Mars">Mars</option>
-                    <option value="Avril">Avril</option>
-                    <option value="Mais">Mai</option>
-                    <option value="Juin">Juillet</option>
-                    <option value="Aout">Aout</option>
-                    <option value="Septembre">Septembre</option>
-                    <option value="Octobre">Octobre</option>
-                    <option value="Novembre">Novembre</option>
-                    <option value="Decembre">Decembre</option>
-                </select>
-            </p>
-        </div>
-
-        <!--- ajout maquette frais hors forfait --->
-        <div class="panel-body">
-            <form class="form-horizontal" role="form" method="POST"  action="index.php?uc=gererFrais&action=validerMajFraisForfait">
-                <label>Saisie d'un nouveau frais forfaitisé :</label><br/>
-                <label>Type de frais :</label>
-                <select method="POST" name="lstFrais" id="typeFrais"><option selected="choix-Option"> Choisissez un type de frais
-                    <?php foreach ($lesFraisForfait as $libelleFrais)
-                        { ?>
-                    <option value="<? echo $libelleFrais['libelle']?>"><?php echo $libelleFrais['libelle'] ?></option>
-                    <?php
-                    }
-                    ?>
-                </select> <br/>
-
-                <label>Date de l'engagement de la dépense :</label>
-                <form name="Filter" method="POST">
-                    <input type="date" name="dateAjout" value="<?php echo date('Y-m-d'); ?>" />
-                </form>
-                <br/>
-                <label>Description :</label> <input method="POST" name="descriptionInput" type="text"><br/>
-                <label>Quantité :</label> <input method="POST" name="quantitéInput" type="number"><br/>
-                <input class="btn btn-primary" id="ok" type="submit" value="Valider" size="20" <?php if ($lesInfosFicheFrais['idEtat']!='CR') { echo 'disabled';} ?>/> <br/>
-            </form>
-        </div>
-
-        <!-- fin maquette frais hors forfait -->
-
-        </div>
-    </div>
-
-
-
-
-</div>
-
-<div class="col-md-6">
 	<div class="content-box-large">
 		<div class="panel-heading">
 			<legend>Elément hors forfait</legend>
@@ -123,5 +36,37 @@
 						?></tr><?php		 }  ?>	  
 			 </table>
 		</div> 
+	</div>
+</div>
+
+<div class="col-md-6">
+	<div class="content-box-large">
+		<div class="panel-heading">
+			<legend>Nouvel élément hors forfait</legend>			
+		</div>
+		<div class="panel-body">
+			<form class="form-horizontal" role="form" action="index.php?uc=gererFrais&action=validerCreationFrais" method="post">
+				<div class="form-group">
+					<div class="form-group">
+					<label for="txtDateHF"> Date (jj/mm/aaaa): </label>
+					</br>
+					<input class="form-control" type="text" id="txtDateHF" name="dateFrais" <?php if ($lesInfosFicheFrais['idEtat']!='CR') { 		echo 'disabled';};   ?>/>
+					</br></br>
+					<label for="txtLibelleHF">Libellé</label>
+					</br>
+					<input class="form-control" type="text" id="txtLibelleHF" name="libelle"  <?php if ($lesInfosFicheFrais['idEtat']!='CR') { 		echo 'disabled';};   ?>/>
+					</br></br>
+					<label for="txtMontantHF">Montant : </label>
+					</br>
+					<input class="form-control" type="text" id="txtMontantHF" name="montant"  <?php if ($lesInfosFicheFrais['idEtat']!='CR') { 		echo 'disabled';};   ?>/>
+					</br></br>
+					</div>
+				</div>
+			<div class="horizontal-form">
+				<input class="btn btn-primary" id="ajouter" type="submit" value="Ajouter" <?php if ($lesInfosFicheFrais['idEtat']!='CR') { echo 'disabled';} ?> />
+				<input class="btn btn-primary" id="effacer" type="reset" value="Effacer"/>
+            </div>
+			</form>
+		</div>
 	</div>
 </div>
