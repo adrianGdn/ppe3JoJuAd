750d5ec5bbefed9f01c628d5e3622312171aef3d
neiluj76 <julien.petit@epsi.fr>
2017-04-04 15:05:10 +0200

    Ajout du bouton GLPI

--- gsbAppliFraisV2/vues/v_sommaire.php
+++ gsbAppliFraisV2/vues/v_sommaire.php
@@ -41,6 +41,7 @@
                             <li class="active"><a href="index.php?uc=gererFrais&action=saisirFrais">Saisir frais forfaitisé</a></li>
                             <li class="active"><a href="index.php?uc=gererFraisHorsForfait&action=saisirFraisHorsForfait">Saisir frais hors forfait</a></li>
                             <li class="active"><a href="index.php?uc=etatFrais&action=selectionnerMois">Mes fiches de frais</a></li>
+                            <li class="active"><a href="#">GLPI</a></li>
                         </ul>
                         <ul class="nav navbar-nav navbar-right">
                             <li><a><?php echo $_SESSION['prenom']."  ".$_SESSION['nom']; ?></a></li>
