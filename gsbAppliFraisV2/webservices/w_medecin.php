bab4d0e3270ab1b7948191dfc96200f8158ca300
jonathan <jg_972@hotmail.com>
2017-04-07 11:41:06 +0200

    méthode récupération json médecins

--- gsbAppliFraisV2/webservices/w_medecin.php
+++ gsbAppliFraisV2/webservices/w_medecin.php
@@ -1,8 +1,14 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+include("../include/class.pdogsb.inc.php");
+/**
+ * 
+ * Retourne un json contenant la liste des medecins attitrés aux visiteurs
+ * 
+ * @param type $idVisiteur
+ * @return json
  */
-
+function w_getLesMedecins($idVisiteur)
+{
+    return json_encode($pdo->getLesMedecins($idVisiteur));
+}
