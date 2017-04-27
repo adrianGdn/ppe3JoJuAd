63e7a4a381fd0e455612f1ec4d37e0c766a21590
jonathan <jg_972@hotmail.com>
2017-04-27 09:45:04 +0200

    rectif connection bdd

--- gsbAppliFraisV2/include/class.pdogsb.inc.php
+++ gsbAppliFraisV2/include/class.pdogsb.inc.php
@@ -18,9 +18,9 @@
 
 class PdoGsb{   		
       	private static $serveur='mysql:host=localhost';
-      	private static $bdd='dbname=adrian';
+      	private static $bdd='dbname=gsbapplifrais';
       	private static $user='adrian' ; // Pour générer en local sous Windows, utiliser en user 'root' sinon 'adrian' ou 'julien' ou 'jonathan'
-      	private static $mdp='' ; // Pour générer en local sous Windows, laisser mdp vide sinon 'adrian' ou 'julien' ou 'jonathan'
+      	private static $mdp='adrian' ; // Pour générer en local sous Windows, laisser mdp vide sinon 'adrian' ou 'julien' ou 'jonathan'
 		private static $monPdo;
 		private static $monPdoGsb=null;
 
