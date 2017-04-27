880d059152fdb2e45884dd3ad85efeaaa1c9cddc
jonathan <jg_972@hotmail.com>
2017-04-27 09:57:51 +0200

    nettoyage v_listeFF

--- gsbAppliFraisV2/vues/v_listeFraisForfait.php
+++ gsbAppliFraisV2/vues/v_listeFraisForfait.php
@@ -60,28 +60,5 @@ echo(var_dump($lesInfosFicheFrais));
             </form>
         </div>      
     </div>
-
-		</div>
-		<div class="panel-body">
-			<form class="form-horizontal" role="form" method="POST"  action="index.php?uc=gererFrais&action=validerMajFraisForfait">
-			    <div class="form-group">
-                    <?php
-                        foreach ($lesFraisForfait as $unFrais)
-                        {
-                            $idFrais = $unFrais['idfrais'];
-                            $libelle = $unFrais['libelle'];
-                            $quantite = $unFrais['quantite'];
-                    ?>
-                    <div class="form-group">
-					    <label for="idFrais"><?php echo $libelle ?></label>
-					    <input class="form-control" placeholder="<?php echo $quantite?>" type="text" id="idFrais" name="lesFrais[<?php echo $idFrais?>]""<?php echo $quantite?>"<?php if ($lesInfosFicheFrais['idEtat']!='CR') { echo 'disabled';} ?> />
-					</div>
-					<?php
-						}
-					?>
-				</div>
-				<input class="btn btn-primary" id="ok" type="submit" value="Valider" size="20" <?php if ($lesInfosFicheFrais['idEtat']!='CR') { echo 'disabled';} ?>/>
-            </form>
-        </div>
-    </div>
+</div>
 </div>
