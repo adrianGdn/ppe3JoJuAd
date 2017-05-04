5e609d29acc3436c28b014f6c597fe1a79a065ed
adrianGdn <adrian.gandon@epsi.fr>
2017-05-04 18:28:27 +0100

    Màj web service

--- gsbAppliFraisV2/webservices/w_visite.php
+++ gsbAppliFraisV2/webservices/w_visite.php
@@ -1,14 +1,25 @@
 <?php
 
-/**
- * 
- * Retourne un json de toutes les visites attribuées au visiteur dont l'id est passé en paramètre 
- * 
- * @param type $idVisiteur
- * @return json
- */
-function w_getLesVisites($idVisiteur)
-{
-    return json_encode($pdo->getLesVisiteur($idVisiteur));
-}
 
+/**
+ * Retourne un json de toutes les visites
+ *
+ * @return string Retourne du JSON
+ */
+include("../include/class.pdogsb.inc.php");
+
+if(isset($_GET['user']))//requière l'id de l'utilisateur en paramètre
+    $idActeur = ($_GET['user']);//assigne l'id de l'utilisateur à la variable pour la requête sql
+//définit une instance de classe pdo de façon à pouvoir appeler les méthodes de la classe pdo
+//une instaniation classique avec new est impossible à cause de l'implémentation de singleton dans pdogsb
+$pdo = PdoGsb::getPdoGsb();
+
+//récupère les medecins de la BDD
+$tabVisites = $pdo->getLesVisites($idActeur);
+
+//renvoi en json
+
+header('Content-type: application/json');
+echo json_encode(array('visite'=>$tabVisites));
+
+?>
